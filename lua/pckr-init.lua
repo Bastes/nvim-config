fe647ba0832d8b6e0466669e310ed7507395a253
Michel Belleville <michel.belleville@gmail.com>
2025-01-29 15:12:48 +0100

    goodbye stylus, I loved you but you're not used by anyone anymore

--- lua/pckr-init.lua
+++ lua/pckr-init.lua
@@ -21,7 +21,6 @@ require('pckr').add{
    'David-Kunz/gen.nvim',
   -- syntaxes
   'tpope/vim-rails';
-  'wavded/vim-stylus';
   'chrisbra/csv.vim';
   { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' };
   'nvim-treesitter/nvim-treesitter-textobjects';
