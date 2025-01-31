6bba2857e4a4dd7461d6974ef8eb4cb2109b2913
Michel Belleville <michel.belleville@gmail.com>
2025-01-31 16:22:20 +0100

    ultisnips configured using lua

---
+++ lua/ultisnips-init.lua
@@ -0,0 +1,8 @@
+-- Trigger configuration. You need to change this to something other than <tab> if you use one of the following:
+-- - https://github.com/Valloric/YouCompleteMe
+-- - https://github.com/nvim-lua/completion-nvim
+vim.g.UltiSnipsJumpForwardTrigger = "<Tab>"
+vim.g.UltiSnipsJumpBackwardTrigger = "<S-Tab>"
+
+-- If you want :UltiSnipsEdit to split your window.
+vim.g.UltiSnipsEditSplit = "vertical"
--- plugins/ultisnips.vim
+++
@@ -1,8 +0,0 @@
-" Trigger configuration. You need to change this to something other than <tab> if you use one of the following:
-" - https://github.com/Valloric/YouCompleteMe
-" - https://github.com/nvim-lua/completion-nvim
-let g:UltiSnipsJumpForwardTrigger="<Tab>"
-let g:UltiSnipsJumpBackwardTrigger="<S-Tab>"
-
-" If you want :UltiSnipsEdit to split your window.
-let g:UltiSnipsEditSplit="vertical"
