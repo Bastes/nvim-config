e244d02056a696f1be182995aed900ab87026906
Michel Belleville <michel.belleville@gmail.com>
2025-02-10 11:59:03 +0100

    WIP this does not quite work (does not plug into native lsp)

---
+++ lua/plugins/typescript-tools.lua
@@ -0,0 +1,10 @@
+return {
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  opts = {},
+  config = function()
+    require('typescript-tools').setup({
+      capabilities = require('cmp_nvim_lsp').default_capabilities()
+    })
+  end,
+}
