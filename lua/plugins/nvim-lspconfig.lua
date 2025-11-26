736b2e21f06e14a17132814d27ea3612e508d4ab
Michel Belleville <michel.belleville@gmail.com>
2025-11-26 23:46:26 +0100

    introducing lua_ls to the mix and using it for better readability

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -7,9 +7,24 @@ return {
     cmd = { "LspInfo", "LspInstall", "LspUninstall" },
 
     config = function()
-      vim.keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, { noremap = true, silent = true, desc = "goto [D]eclaration" })
-      vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { noremap = true, silent = true, desc = "goto [d]efinition" })
-      vim.keymap.set("n", "<leader>lA", vim.lsp.buf.code_action, { noremap = true, silent = true, desc = "code [A]ction" })
+      vim.keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, {
+        noremap = true,
+        silent = true,
+        desc = "goto [D]eclaration"
+      })
+
+      vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, {
+        noremap = true,
+        silent = true,
+        desc = "goto [d]efinition"
+      })
+
+      vim.keymap.set("n", "<leader>lA", vim.lsp.buf.code_action, {
+        noremap = true,
+        silent = true,
+        desc = "code [A]ction"
+      })
+
       vim.keymap.set("n", "<leader>la", function()
         vim.lsp.buf.code_action({
           filter = function(action)
@@ -17,18 +32,49 @@ return {
           end,
           apply = true,
         })
-      end, { noremap = true, silent = true, desc = "quick fix (code [a]ction)" })
-      vim.keymap.set("n", "<leader>le", vim.diagnostic.open_float, { noremap = true, silent = true, desc = "[e]rror diagnostic" })
-      vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { noremap = true, silent = true, desc = "[h]over" })
+      end, {
+        noremap = true,
+        silent = true,
+        desc = "quick fix (code [a]ction)"
+      })
+
+      vim.keymap.set("n", "<leader>le", vim.diagnostic.open_float, {
+        noremap = true,
+        silent = true,
+        desc = "[e]rror diagnostic"
+      })
+
+      vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, {
+        noremap = true,
+        silent = true,
+        desc = "[h]over"
+      })
+
       vim.keymap.set("n", "<leader>li", function()
         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-      end, {noremap = true, silent = true, desc = "toggle [i]nlay hints"})
+      end, {
+        noremap = true,
+        silent = true,
+        desc = "toggle [i]nlay hints",
+      })
 
-      vim.keymap.set("n", "<leader>ls", vim.lsp.buf.document_symbol, { noremap = true, silent = true, desc = "document [s]ymbol" })
-      vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, { noremap = true, silent = true, desc = "find [r]eferences" })
-      vim.keymap.set("n", "<leader>lR", vim.lsp.buf.rename, { noremap = true, silent = true, desc = "[R]ename symbol" })
+      vim.keymap.set("n", "<leader>ls", vim.lsp.buf.document_symbol, {
+        noremap = true,
+        silent = true,
+        desc = "document [s]ymbol"
+      })
 
-      vim.opt.signcolumn = "yes"
+      vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, {
+        noremap = true,
+        silent = true,
+        desc = "find [r]eferences"
+      })
+
+      vim.keymap.set("n", "<leader>lR", vim.lsp.buf.rename, {
+        noremap = true,
+        silent = true,
+        desc = "[R]ename symbol"
+      })
 
       vim.lsp.config("ruby_lsp", {
         init_options = {
@@ -56,6 +102,7 @@ return {
         },
       })
       vim.lsp.enable("gopls")
+      vim.lsp.enable("lua_ls")
     end,
   },
 }
