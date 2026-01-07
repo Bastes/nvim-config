1166807458ed86890c8f1aba4a438860d6364082
Michel Belleville <michel.belleville@gmail.com>
2026-01-07 11:25:54 +0100

    it's time gv got his <leader> mappings

--- lua/plugins/gv.lua
+++ lua/plugins/gv.lua
@@ -1,1 +1,8 @@
-return { 'junegunn/gv.vim' }
+return {
+  'junegunn/gv.vim',
+  keys = {
+    { '<leader>gH',  '<cmd>GV<cr>',  desc = 'Git [H]istory view' },
+    { '<leader>ghf', '<cmd>GV!<cr>', desc = '[f]ile-specific history view' },
+    { '<leader>ghl', '<cmd>GV?<cr>', desc = 'Fill location [l]ist with revisions' },
+  }
+}
