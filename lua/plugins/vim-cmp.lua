903544b4e62bbbdb6337ae6d0030b05bbeed2ddc
Michel Belleville <michel.belleville@gmail.com>
2026-06-04 11:43:32 +0200

    now with emoji capabilities!

--- lua/plugins/vim-cmp.lua
+++ lua/plugins/vim-cmp.lua
@@ -28,7 +28,7 @@ return {
       mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         ["<Tab>"] = cmp.mapping(
@@ -47,13 +47,15 @@ return {
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'ultisnips' },
-        { name = 'buffer',
+        {
+          name = 'buffer',
           option = {
             get_bufnrs = function()
               return vim.api.nvim_list_bufs()
             end
           }
-        }
+        },
+        { name = 'emoji' },
       })
     })
 
@@ -88,4 +90,3 @@ return {
     })
   end
 }
-
